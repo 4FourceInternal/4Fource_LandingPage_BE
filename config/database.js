d9641f2af729c4723e220fedcda1ca47b023d2af
Freezeyy <fareezhishamuddin@gmail.com>
2025-09-19 00:53:39 +0800

    and again

--- config/database.js
+++ config/database.js
@@ -1,17 +1,14 @@
 module.exports = ({ env }) => ({
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-      options: {}
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false), // set to false if using local DB
     },
+    debug: false,
   },
 });
