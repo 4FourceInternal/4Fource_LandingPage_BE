b77ce1646e73cb789e9758fb59609fc16312f057
Freezeyy <fareezhishamuddin@gmail.com>
2025-09-19 12:46:48 +0800

    [UPDATE] package/server

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS', ['defaultKey1', 'defaultKey2', 'defaultKey3', 'defaultKey4']),
   },
+  url: env('PUBLIC_URL', `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`),
+  proxy: true,
+  cron: {
+    enabled: false,
+  },
 });
