9af8e122201186d0a95a7cc7687b871ba93ee413
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2025-09-27 00:30:20 +0800

    add backgroundimage in service card

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,10 +1,10 @@
-import type { Schema, Struct } from '@strapi/strapi';
+import type { Schema, Struct } from "@strapi/strapi";
 
 export interface SharedApproach extends Struct.ComponentSchema {
-  collectionName: 'components_shared_approach';
+  collectionName: "components_shared_approach";
   info: {
-    description: 'Approach section component';
-    displayName: 'Approach';
+    description: "Approach section component";
+    displayName: "Approach";
   };
   attributes: {
     items: Schema.Attribute.JSON & Schema.Attribute.Required;
@@ -13,36 +13,36 @@ export interface SharedApproach extends Struct.ComponentSchema {
 }
 
 export interface SharedBottomCta extends Struct.ComponentSchema {
-  collectionName: 'components_shared_bottom_cta';
+  collectionName: "components_shared_bottom_cta";
   info: {
-    description: 'Bottom call to action component';
-    displayName: 'Bottom CTA';
+    description: "Bottom call to action component";
+    displayName: "Bottom CTA";
   };
   attributes: {
-    buttons: Schema.Attribute.Component<'shared.button', true>;
+    buttons: Schema.Attribute.Component<"shared.button", true>;
     lead: Schema.Attribute.Text & Schema.Attribute.Required;
     title: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
 export interface SharedBrand extends Struct.ComponentSchema {
-  collectionName: 'components_shared_brand';
+  collectionName: "components_shared_brand";
   info: {
-    description: 'Brand information for header';
-    displayName: 'Brand';
+    description: "Brand information for header";
+    displayName: "Brand";
   };
   attributes: {
     logoText: Schema.Attribute.String &
       Schema.Attribute.Required &
-      Schema.Attribute.DefaultTo<'Qoyy Global'>;
+      Schema.Attribute.DefaultTo<"Qoyy Global">;
   };
 }
 
 export interface SharedButton extends Struct.ComponentSchema {
-  collectionName: 'components_shared_button';
+  collectionName: "components_shared_button";
   info: {
-    description: 'Button component';
-    displayName: 'Button';
+    description: "Button component";
+    displayName: "Button";
   };
   attributes: {
     href: Schema.Attribute.String & Schema.Attribute.Required;
@@ -51,22 +51,22 @@ export interface SharedButton extends Struct.ComponentSchema {
 }
 
 export interface SharedContactButtons extends Struct.ComponentSchema {
-  collectionName: 'components_shared_contact_buttons';
+  collectionName: "components_shared_contact_buttons";
   info: {
-    description: 'Contact buttons component';
-    displayName: 'Contact Buttons';
+    description: "Contact buttons component";
+    displayName: "Contact Buttons";
   };
   attributes: {
-    email: Schema.Attribute.Component<'shared.button', false>;
-    whatsapp: Schema.Attribute.Component<'shared.button', false>;
+    email: Schema.Attribute.Component<"shared.button", false>;
+    whatsapp: Schema.Attribute.Component<"shared.button", false>;
   };
 }
 
 export interface SharedContactInfo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_contact_info';
+  collectionName: "components_shared_contact_info";
   info: {
-    description: 'Contact information component';
-    displayName: 'Contact Info';
+    description: "Contact information component";
+    displayName: "Contact Info";
   };
   attributes: {
     addressLines: Schema.Attribute.JSON & Schema.Attribute.Required;
@@ -77,23 +77,23 @@ export interface SharedContactInfo extends Struct.ComponentSchema {
 }
 
 export interface SharedCta extends Struct.ComponentSchema {
-  collectionName: 'components_shared_cta';
+  collectionName: "components_shared_cta";
   info: {
-    description: 'Call to action component';
-    displayName: 'CTA';
+    description: "Call to action component";
+    displayName: "CTA";
   };
   attributes: {
-    button: Schema.Attribute.Component<'shared.button', false>;
+    button: Schema.Attribute.Component<"shared.button", false>;
     lead: Schema.Attribute.Text & Schema.Attribute.Required;
     title: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
 export interface SharedFaq extends Struct.ComponentSchema {
-  collectionName: 'components_shared_faq';
+  collectionName: "components_shared_faq";
   info: {
-    description: 'FAQ item component';
-    displayName: 'FAQ';
+    description: "FAQ item component";
+    displayName: "FAQ";
   };
   attributes: {
     answer: Schema.Attribute.Text & Schema.Attribute.Required;
@@ -102,28 +102,28 @@ export interface SharedFaq extends Struct.ComponentSchema {
 }
 
 export interface SharedFooter extends Struct.ComponentSchema {
-  collectionName: 'components_shared_footer';
+  collectionName: "components_shared_footer";
   info: {
-    description: 'Footer component for global settings';
-    displayName: 'Footer';
+    description: "Footer component for global settings";
+    displayName: "Footer";
   };
   attributes: {
     companyName: Schema.Attribute.String &
       Schema.Attribute.Required &
-      Schema.Attribute.DefaultTo<'Qoyy Global'>;
-    contactInfo: Schema.Attribute.Component<'shared.contact-info', false>;
+      Schema.Attribute.DefaultTo<"Qoyy Global">;
+    contactInfo: Schema.Attribute.Component<"shared.contact-info", false>;
     copyright: Schema.Attribute.Text &
       Schema.Attribute.Required &
-      Schema.Attribute.DefaultTo<'COPYRIGHT \u24B8 2025 QOYY GLOBAL (002857086-D)\u200D\nAll rights reserved.'>;
-    quickLinks: Schema.Attribute.Component<'shared.footer-link', true>;
+      Schema.Attribute.DefaultTo<"COPYRIGHT \u24B8 2025 QOYY GLOBAL (002857086-D)\u200D\nAll rights reserved.">;
+    quickLinks: Schema.Attribute.Component<"shared.footer-link", true>;
   };
 }
 
 export interface SharedFooterLink extends Struct.ComponentSchema {
-  collectionName: 'components_shared_footer_link';
+  collectionName: "components_shared_footer_link";
   info: {
-    description: 'Footer navigation link';
-    displayName: 'Footer Link';
+    description: "Footer navigation link";
+    displayName: "Footer Link";
   };
   attributes: {
     label: Schema.Attribute.String & Schema.Attribute.Required;
@@ -132,46 +132,46 @@ export interface SharedFooterLink extends Struct.ComponentSchema {
 }
 
 export interface SharedHeader extends Struct.ComponentSchema {
-  collectionName: 'components_shared_header';
+  collectionName: "components_shared_header";
   info: {
-    description: 'Header component for global settings';
-    displayName: 'Header';
+    description: "Header component for global settings";
+    displayName: "Header";
   };
   attributes: {
-    brand: Schema.Attribute.Component<'shared.brand', false>;
-    navLinks: Schema.Attribute.Component<'shared.nav-link', true>;
+    brand: Schema.Attribute.Component<"shared.brand", false>;
+    navLinks: Schema.Attribute.Component<"shared.nav-link", true>;
   };
 }
 
 export interface SharedHero extends Struct.ComponentSchema {
-  collectionName: 'components_shared_hero';
+  collectionName: "components_shared_hero";
   info: {
-    description: 'Hero section component';
-    displayName: 'Hero';
+    description: "Hero section component";
+    displayName: "Hero";
   };
   attributes: {
-    backgroundImage: Schema.Attribute.Media<'images'>;
+    backgroundImage: Schema.Attribute.Media<"images">;
     title1: Schema.Attribute.String & Schema.Attribute.Required;
     title2: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
 export interface SharedMedia extends Struct.ComponentSchema {
-  collectionName: 'components_shared_media';
+  collectionName: "components_shared_media";
   info: {
-    displayName: 'Media';
-    icon: 'file-video';
+    displayName: "Media";
+    icon: "file-video";
   };
   attributes: {
-    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+    file: Schema.Attribute.Media<"images" | "files" | "videos">;
   };
 }
 
 export interface SharedNavLink extends Struct.ComponentSchema {
-  collectionName: 'components_shared_nav_link';
+  collectionName: "components_shared_nav_link";
   info: {
-    description: 'Navigation link for header';
-    displayName: 'Navigation Link';
+    description: "Navigation link for header";
+    displayName: "Navigation Link";
   };
   attributes: {
     label: Schema.Attribute.String & Schema.Attribute.Required;
@@ -180,10 +180,10 @@ export interface SharedNavLink extends Struct.ComponentSchema {
 }
 
 export interface SharedOffer extends Struct.ComponentSchema {
-  collectionName: 'components_shared_offer';
+  collectionName: "components_shared_offer";
   info: {
-    description: 'Offer item component';
-    displayName: 'Offer';
+    description: "Offer item component";
+    displayName: "Offer";
   };
   attributes: {
     label: Schema.Attribute.String & Schema.Attribute.Required;
@@ -192,10 +192,10 @@ export interface SharedOffer extends Struct.ComponentSchema {
 }
 
 export interface SharedProcess extends Struct.ComponentSchema {
-  collectionName: 'components_shared_process';
+  collectionName: "components_shared_process";
   info: {
-    description: 'Process steps component';
-    displayName: 'Process';
+    description: "Process steps component";
+    displayName: "Process";
   };
   attributes: {
     steps: Schema.Attribute.JSON & Schema.Attribute.Required;
@@ -204,10 +204,10 @@ export interface SharedProcess extends Struct.ComponentSchema {
 }
 
 export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
+  collectionName: "components_shared_quotes";
   info: {
-    displayName: 'Quote';
-    icon: 'indent';
+    displayName: "Quote";
+    icon: "indent";
   };
   attributes: {
     body: Schema.Attribute.Text;
@@ -216,11 +216,11 @@ export interface SharedQuote extends Struct.ComponentSchema {
 }
 
 export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
+  collectionName: "components_shared_rich_texts";
   info: {
-    description: '';
-    displayName: 'Rich text';
-    icon: 'align-justify';
+    description: "";
+    displayName: "Rich text";
+    icon: "align-justify";
   };
   attributes: {
     body: Schema.Attribute.RichText;
@@ -228,76 +228,80 @@ export interface SharedRichText extends Struct.ComponentSchema {
 }
 
 export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
+  collectionName: "components_shared_seos";
   info: {
-    description: '';
-    displayName: 'Seo';
-    icon: 'allergies';
-    name: 'Seo';
+    description: "";
+    displayName: "Seo";
+    icon: "allergies";
+    name: "Seo";
   };
   attributes: {
     metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
     metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    shareImage: Schema.Attribute.Media<'images'>;
+    shareImage: Schema.Attribute.Media<"images">;
   };
 }
 
 export interface SharedServiceCard extends Struct.ComponentSchema {
-  collectionName: 'components_shared_service_card';
+  collectionName: "components_shared_service_card";
   info: {
-    description: 'Service card component';
-    displayName: 'Service Card';
+    description: "Service card component";
+    displayName: "Service Card";
   };
   attributes: {
     description: Schema.Attribute.Text & Schema.Attribute.Required;
+    backgroundImage: Schema.Attribute.Media<
+      "images" | "files" | "videos" | "audios"
+    > &
+      Schema.Attribute.Required;
     features: Schema.Attribute.JSON;
-    testing: Schema.Attribute.Text
+    testing: Schema.Attribute.Text;
     icon: Schema.Attribute.String;
     title: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
 export interface SharedShareHero extends Struct.ComponentSchema {
-  collectionName: 'components_shared_share_heroes';
+  collectionName: "components_shared_share_heroes";
   info: {
-    displayName: 'share.hero';
+    displayName: "share.hero";
   };
   attributes: {};
 }
 
 export interface SharedSharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_shared_seos';
+  collectionName: "components_shared_shared_seos";
   info: {
-    displayName: 'shared.seo';
+    displayName: "shared.seo";
   };
   attributes: {};
 }
 
 export interface SharedSharedseo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sharedseos';
+  collectionName: "components_shared_sharedseos";
   info: {
-    displayName: 'sharedseo';
+    displayName: "sharedseo";
   };
   attributes: {};
 }
 
 export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
+  collectionName: "components_shared_sliders";
   info: {
-    description: '';
-    displayName: 'Slider';
-    icon: 'address-book';
+    description: "";
+    displayName: "Slider";
+    icon: "address-book";
   };
   attributes: {
-    files: Schema.Attribute.Media<'images', true>;
+    files: Schema.Attribute.Media<"images", true>;
   };
 }
 
 export interface SharedWhyUs extends Struct.ComponentSchema {
-  collectionName: 'components_shared_why_us';
+  collectionName: "components_shared_why_us";
   info: {
-    description: 'Why us section component';
-    displayName: 'Why Us';
+    description: "Why us section component";
+    displayName: "Why Us";
   };
   attributes: {
     points: Schema.Attribute.JSON & Schema.Attribute.Required;
@@ -305,34 +309,34 @@ export interface SharedWhyUs extends Struct.ComponentSchema {
   };
 }
 
-declare module '@strapi/strapi' {
+declare module "@strapi/strapi" {
   export module Public {
     export interface ComponentSchemas {
-      'shared.approach': SharedApproach;
-      'shared.bottom-cta': SharedBottomCta;
-      'shared.brand': SharedBrand;
-      'shared.button': SharedButton;
-      'shared.contact-buttons': SharedContactButtons;
-      'shared.contact-info': SharedContactInfo;
-      'shared.cta': SharedCta;
-      'shared.faq': SharedFaq;
-      'shared.footer': SharedFooter;
-      'shared.footer-link': SharedFooterLink;
-      'shared.header': SharedHeader;
-      'shared.hero': SharedHero;
-      'shared.media': SharedMedia;
-      'shared.nav-link': SharedNavLink;
-      'shared.offer': SharedOffer;
-      'shared.process': SharedProcess;
-      'shared.quote': SharedQuote;
-      'shared.rich-text': SharedRichText;
-      'shared.seo': SharedSeo;
-      'shared.service-card': SharedServiceCard;
-      'shared.share-hero': SharedShareHero;
-      'shared.shared-seo': SharedSharedSeo;
-      'shared.sharedseo': SharedSharedseo;
-      'shared.slider': SharedSlider;
-      'shared.why-us': SharedWhyUs;
+      "shared.approach": SharedApproach;
+      "shared.bottom-cta": SharedBottomCta;
+      "shared.brand": SharedBrand;
+      "shared.button": SharedButton;
+      "shared.contact-buttons": SharedContactButtons;
+      "shared.contact-info": SharedContactInfo;
+      "shared.cta": SharedCta;
+      "shared.faq": SharedFaq;
+      "shared.footer": SharedFooter;
+      "shared.footer-link": SharedFooterLink;
+      "shared.header": SharedHeader;
+      "shared.hero": SharedHero;
+      "shared.media": SharedMedia;
+      "shared.nav-link": SharedNavLink;
+      "shared.offer": SharedOffer;
+      "shared.process": SharedProcess;
+      "shared.quote": SharedQuote;
+      "shared.rich-text": SharedRichText;
+      "shared.seo": SharedSeo;
+      "shared.service-card": SharedServiceCard;
+      "shared.share-hero": SharedShareHero;
+      "shared.shared-seo": SharedSharedSeo;
+      "shared.sharedseo": SharedSharedseo;
+      "shared.slider": SharedSlider;
+      "shared.why-us": SharedWhyUs;
     }
   }
 }
