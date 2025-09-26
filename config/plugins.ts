1e4ef505b64da01539b33cd12558fd05c34e4964
Mohamad Safwan <mohamadsafwan.1905@gmail.com>
2025-09-26 23:51:23 +0800

    fix: increase size limit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      sizeLimit: 200 * 1024 * 1024, // 200MB in bytes
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+    },
+  },
+});
